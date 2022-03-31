68c03dc3261c37b6b79bdddfa5c62e5a8310deb8
Xinzhenghang <83825826+Xinzhenghang@users.noreply.github.com>
2022-03-31 15:58:41 +0800

    Update StudentModel.java

--- StudentModel.java
+++ StudentModel.java
@@ -1,1 +1,30 @@
 
+public class StudentModel {
+	
+	
+	public int ID;
+	
+	public String Name;
+	
+	public int Math_Mark;
+	
+	public int Science_Mark;
+	
+	StudentModel(int ID,String Name,int Math_Mark,int Science_Mark ){
+		
+		this.ID=ID;
+		
+		
+		this.Name=Name;
+		
+		
+		this.Math_Mark=Math_Mark;
+		
+		this.Science_Mark=Science_Mark;
+		
+		
+		
+	}
+
+}
+
